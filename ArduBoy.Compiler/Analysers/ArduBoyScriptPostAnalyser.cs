b09acab28c8ba78a9582d23dc105c981200047a8
Kristian Skov <kris701kj@gmail.com>
2024-08-15 09:51:35 +0200

    Added some more prechecks

--- ArduBoy.Compiler/Analysers/ArduBoyScriptPostAnalyser.cs
+++ ArduBoy.Compiler/Analysers/ArduBoyScriptPostAnalyser.cs
@@ -2,6 +2,7 @@
 using ArduBoy.Compiler.Models.Exceptions;
 using ArduBoy.Compiler.Models.Script;
 using ArduBoy.Compiler.Models.Script.Expressions;
+using ArduBoy.Compiler.Models.Script.Expressions.Arithmetic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,11 +26,26 @@ namespace ArduBoy.Compiler.Analysers
 
 		private void CheckIfAssignmentsAreValid(ArduBoyScriptDefinition from)
 		{
-			var setExps = from.FindTypes<SetExp>();
-			foreach(var set in setExps)
+			foreach(var set in from.FindTypes<SetExp>())
 				if (set.Value is ValueExpression val)
 					if (!int.TryParse(val.Value, out int tmp))
 						throw new PostAnalyserException(set, "Variable of set expression is not a integer!");
+			foreach (var add in from.FindTypes<AddExp>())
+				if (add.Value is ValueExpression val)
+					if (!int.TryParse(val.Value, out int tmp))
+						throw new PostAnalyserException(add, "Variable of add expression is not a integer!");
+			foreach (var sub in from.FindTypes<SubExp>())
+				if (sub.Value is ValueExpression val)
+					if (!int.TryParse(val.Value, out int tmp))
+						throw new PostAnalyserException(sub, "Variable of sub expression is not a integer!");
+			foreach (var div in from.FindTypes<DivExp>())
+				if (div.Value is ValueExpression val)
+					if (!int.TryParse(val.Value, out int tmp))
+						throw new PostAnalyserException(div, "Variable of div expression is not a integer!");
+			foreach (var mult in from.FindTypes<MultExp>())
+				if (mult.Value is ValueExpression val)
+					if (!int.TryParse(val.Value, out int tmp))
+						throw new PostAnalyserException(mult, "Variable of molt expression is not a integer!");
 		}
 	}
 }
